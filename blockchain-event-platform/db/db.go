728aa2b166b2235238d47776907fb666de9f23f9
Balamagali Prasanna <balamagali332@gmail.com>
2026-03-22 11:21:02 +0530

    Update db.go

--- blockchain-event-platform/db/db.go
+++ blockchain-event-platform/db/db.go
@@ -7,31 +7,28 @@ import (
 	_ "github.com/lib/pq"
 )
 
-var WriteDB *sql.DB // primary DB (for writes)
-var ReadDB *sql.DB  // replica DB (for reads)
+var WriteDB *sql.DB 
+var ReadDB *sql.DB 
 
 func Init() {
 	var err error
 
-	// Connect to primary DB (write)
+	
 	WriteDB, err = sql.Open("postgres", "postgres://myuser:mypassword@localhost:5432/blockchain?sslmode=disable")
 	if err != nil {
 		log.Fatal("Failed to connect primary DB:", err)
 	}
 
-	// Test the connection
 	err = WriteDB.Ping()
 	if err != nil {
 		log.Fatal("Cannot ping primary DB:", err)
 	}
 
-	// Connect to replica DB (read)
 	ReadDB, err = sql.Open("postgres", "postgres://myuser:mypassword@localhost:5432/blockchain_read?sslmode=disable")
 	if err != nil {
 		log.Fatal("Failed to connect replica DB:", err)
 	}
 
-	// Test the connection
 	err = ReadDB.Ping()
 	if err != nil {
 		log.Fatal("Cannot ping replica DB:", err)
